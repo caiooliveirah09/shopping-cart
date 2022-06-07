2f9d556fb5f50be40012b8e5d90b185b29619aa6
caiooliveirah09 <leelros@hotmail.com>
2022-06-07 15:06:58 -0300

    i solved requirement 3, 4

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-const createProductImageElement = (imageSource) => {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
+// coloca minhas seções na página;
+const appendChild = (parent, child) => {
+  const element = document.querySelector(parent);
+  element.appendChild(child);
 };
-
-const createCustomElement = (element, className, innerText) => {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-};
-
-const createProductItemElement = ({ sku, name, image, price }) => {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-// Adicionei um span onde vai o preço do item, embaixo da imagem;
-  section.appendChild(createCustomElement('span', 'item_price', `R$ ${price}`));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-};
-
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
   // coloque seu código aqui
@@ -40,24 +16,77 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-// MINHAS FUNÇÕES;
-
-// Coloca minhas seções na página;
-const appendChild = (parent, child) => {
-  const element = document.querySelector(parent);
-  element.appendChild(child);
+// função q pega as informações do item clicado, e cria um objeto com nome, id e preço;
+const addCart = async (event) => {
+  const item = event.target.parentElement.children[0].innerText;
+  const response = await fetchItem(item);
+  const object = {
+    sku: response.id,
+    name: response.title,
+    salePrice: response.price,
+  };
+// coloca no carrinho;
+  const cartItem = createCartItemElement(object);
+  appendChild('.cart__items', cartItem);
 };
 
-// Organiza os itens trazidos do json da API em sku, name, image, price;
+const createProductImageElement = (imageSource) => {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+};
+// adicionei uma possivel função ao elemento;
+const createCustomElement = (element, className, innerText, func) => {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+
+  // coloca um evento no botão q eu quiser; 
+  e.onclick = func;
+  return e;
+};
+
+/* const createCustomElementButton = (element, className, innerText, func) => {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  e.onclick = func;
+  return e;
+};
+*/
+
+const createProductItemElement = ({ sku, name, image, salePrice }) => {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  // adicionei um span onde vai o preço do item, embaixo da imagem;
+  section.appendChild(createCustomElement('span', 'item_price', `R$ ${salePrice}`));
+  section.appendChild(createCustomElement('button',
+    'item__add',
+    'Adicionar ao carrinho!',
+    addCart));
+  return section;
+};
+
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+// MINHAS FUNÇÕES;
+
+// organiza os itens trazidos do json da API em sku, name, image, price;
 const items = async () => {
   const response = await fetchProducts('computador');
-  const result = response.map((item) => ({ sku: item.id,
+  const result = response.map((item) => ({
+    sku: item.id,
     name: item.title,
     image: item.thumbnail,
-    price: item.price,
-   }));
+    salePrice: item.price,
+  }));
 
-// createProductItemElement cria minhas seções e o appendChild coloca eles dentro da minha página;
+  // createProductItemElement cria minhas seções e o appendChild coloca eles dentro da minha página;
   result.forEach((item) => {
     appendChild('.items', createProductItemElement(item));
   });
